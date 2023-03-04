0f2910ecd9e942319602ea41135f6a66235d7b5f
seyityahya <seyityahya05@gmail.com>
2023-03-04 18:16:23 +0300

    project finish

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -3,7 +3,7 @@ import { getContrastYIQ } from "../helper";
 import MainContext from "../MainContext";
 import ClipboardButton from "react-clipboard.js";
 
-function Brand({ brand }) {
+function Brand({ brand, style }) {
   const { selectedBrands, setSelectedBrands, setCopied } =
     useContext(MainContext);
 
@@ -19,26 +19,28 @@ function Brand({ brand }) {
   };
 
   return (
-    <div
-      className={`brand ${
-        selectedBrands.includes(brand.slug) ? "selected" : ""
-      }`}
-    >
-      <h5 onClick={toggleSelected}>{brand.title}</h5>
-      <div className="brand-colors">
-        {brand.colors.map((color) => (
-          <ClipboardButton
-            data-clipboard-text={color}
-            onSuccess={() => setColor(color)}
-            component="span"
-            style={{
-              "--bgColor": `#${color}`,
-              "--textColor": `${getContrastYIQ(color)}`,
-            }}
-          >
-            {color}
-          </ClipboardButton>
-        ))}
+    <div style={style}>
+      <div
+        className={`brand ${
+          selectedBrands.includes(brand.slug) ? "selected" : ""
+        }`}
+      >
+        <h5 onClick={toggleSelected}>{brand.title}</h5>
+        <div className="brand-colors">
+          {brand.colors.map((color) => (
+            <ClipboardButton
+              data-clipboard-text={color}
+              onSuccess={() => setColor(color)}
+              component="span"
+              style={{
+                "--bgColor": `#${color}`,
+                "--textColor": `${getContrastYIQ(color)}`,
+              }}
+            >
+              {color}
+            </ClipboardButton>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/Content.js
+++ src/components/Content.js
@@ -2,12 +2,16 @@ import Search from "./Search";
 import Brand from "./Brand";
 import MainContext from "../MainContext";
 import { useContext } from "react";
-import LazyLoad from "react-lazyload";
 import Dowload from "./Dowload";
+import { List, AutoSizer } from "react-virtualized";
 import Loader from "./Loader";
 
 function Content() {
   const { brands, selectedBrands } = useContext(MainContext);
+
+  const rowRenderer = ({ key, index, style }) => {
+    return <Brand style={style} brand={brands[index]} key={key} />;
+  };
 
   return (
     <main className="content">
@@ -16,16 +20,20 @@ function Content() {
         {selectedBrands.length !== 0 && <Dowload />}
       </header>
       <section className="brands">
-        {brands.map((brand) => (
-          <LazyLoad
-            key={brand.slug}
-            once={true}
-            overflow={true}
-            placeholder={<Loader />}
-          >
-            <Brand brand={brand} />
-          </LazyLoad>
-        ))}
+        {/* {brands.map((brand) => (
+          <Brand brand={brand} />
+        ))} */}
+        <AutoSizer>
+          {({ height, width }) => (
+            <List
+              width={width}
+              height={height}
+              rowCount={brands.length}
+              rowHeight={113}
+              rowRenderer={rowRenderer}
+            />
+          )}
+        </AutoSizer>
       </section>
     </main>
   );
